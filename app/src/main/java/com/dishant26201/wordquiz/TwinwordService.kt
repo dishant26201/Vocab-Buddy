f0c1dd0a7fc0ea6a1c71e8c7b1c3954ce0ea7a59
Dishant Behera <dishant26201@gmail.com>
2021-07-05 16:49:32 +0530

    Version 4.0.1 - security

--- app/src/main/java/com/dishant26201/wordquiz/TwinwordService.kt
+++ app/src/main/java/com/dishant26201/wordquiz/TwinwordService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 
 public interface TwinwordService {
 
-    @Headers("X-Twaip-Key: ejxv9wESxNQ5XFJpS0KQpfUd36rzGcQXEmWfq0QQ+OmxiZ3S8LY1wSYY4AH5EqY9UWx2KxAgRNbZ9N6SUBsNqw==")
+    @Headers("Demo")
     @GET("api/quiz/type1/latest/")
     fun getQuestions(
         @Query("level") level : String,
